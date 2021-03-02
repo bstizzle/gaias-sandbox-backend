ac98631cba4253296b4f216ead90d0c42ec47ba5
bstizzle <whereyouben@gmail.com>
2021-03-02 18:31:33 -0500

    heroku routes/config

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   post "/login", to: "auth#login"
 
+  root to: 'application#home'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
